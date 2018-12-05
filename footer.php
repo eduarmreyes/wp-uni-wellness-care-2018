1f9cd16bd1f050c35e8ef3098663bcfa9c5ef5e3
eduarmreyes <eduar.mreyes@gmail.com>
2018-12-04 21:28:33 -0600

    add assets and dummy content

--- footer.php
+++ footer.php
@@ -11,10 +11,34 @@
 
 ?>
 
-	</div><!-- #content -->
+	<!-- </div>#content -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="footer-nav">
+						<img class="brand-footer" src="<?=get_template_directory_uri();?>/images/uni-brand-white.png" />
+						<ul class="list-unstyled">
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Blog</a></li>
+							<li><a href="#">Contact Us</a></li>
+							<li class="sm-link-wrap">
+								<a class="sm-link" href="#"><i class="fab fa-facebook-square"></i></a>
+								<a class="sm-link" href="#"><i class="fab fa-twitter-square"></i></a>
+								<a class="sm-link" href="#"><i class="fab fa-slack-hash"></i></a>
+							</li>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
+						</ul>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- <footer id="colophon" class="site-footer"> -->
+		<!-- <div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'uni-wellness-care' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -26,9 +50,9 @@
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'uni-wellness-care' ), 'uni-wellness-care', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</div>.site-info -->
+	<!-- </footer>#colophon -->
+<!-- </div>#page -->
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -17,11 +17,45 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- Main CSS -->
+    <!-- <link rel="stylesheet" href="assets/styles/main.css"> -->
+
+		<!-- Palanquin Font -->
+    <link href="https://fonts.googleapis.com/css?family=Palanquin:400,700" rel="stylesheet">
+
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+	<div class="nav-header nav-header-clean">
+		<nav class="ml-auto my-md-0 mr-md-2">
+			<a class="px-3 py-1" href="#">About</a>
+			<a class="px-4 py-1" href="#">Blog</a>
+			<a class="px-2 py-1 sm-link" href="#"><i class="fab fa-facebook-square"></i></a>
+			<a class="px-2 py-1 sm-link" href="#"><i class="fab fa-twitter-square"></i></a>
+			<a class="px-2 py-1 sm-link" href="#"><i class="fab fa-slack-hash"></i></a>
+		</nav>
+		<a class="btn btn-primary" href="#"><b>Contact Us</b></a>
+	</div>
+
+	<div class="lp-hero">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<img class="brand-hero" src="<?=get_template_directory_uri();?>/images/uni-brand-color.png" />
+					<p class="text-large mt-3"><b>Connect</b> with Uni Wellness and provide telemental health designed to support life’s key transitions</p>
+				</div>
+				<div class="col-md-6">
+					<img class="bg-hero" src="<?=get_template_directory_uri();?>/images/hero-img.jpg" />
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!-- <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'uni-wellness-care' ); ?></a>
 
 	<header id="masthead" class="site-header">
@@ -42,9 +76,9 @@
 				?>
 				<p class="site-description"><?php echo $uni_wellness_care_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
+		</div>.site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
+		<!-- <nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'uni-wellness-care' ); ?></button>
 			<?php
 			wp_nav_menu( array(
@@ -52,7 +86,7 @@
 				'menu_id'        => 'primary-menu',
 			) );
 			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+		</nav>#site-navigation -->
+	<!-- </header>#masthead -->
 
-	<div id="content" class="site-content">
+	<!-- <div id="content" class="site-content"> -->
