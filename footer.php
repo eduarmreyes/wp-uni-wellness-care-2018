b33a02fdee67b3eef54b9c68cca3eafe4c5eb325
Eduardo <eduar.mreyes@gmail.com>
2019-03-25 12:57:16 -0400

    allow footer for how it works page

--- footer.php
+++ footer.php
@@ -17,49 +17,43 @@
 		</script>
 
 		<!-- </div>#content -->
-		<?php
-			if (str_replace( ' ', '-', strtolower( get_the_title() ) ) !== "how-it-works") {
-		?>
-			<footer>
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<div class="footer-nav">
-								<a href="<?= get_site_url(); ?>">
-									<img class="brand-footer" src="<?=get_template_directory_uri();?>/images/uni-brand-white.png" />
-								</a>
-								<ul class="list-unstyled hovered-opacity-off no-decoration">
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="footer-nav">
+							<a href="<?= get_site_url(); ?>">
+								<img class="brand-footer" src="<?=get_template_directory_uri();?>/images/uni-brand-white.png" />
+							</a>
+							<ul class="list-unstyled hovered-opacity-off no-decoration">
+								<?php
+									$menu_options = wp_get_nav_menu_items('landing navigation footer');
+									foreach ((array) $menu_options as $menu) {
+										?>
+										<li>
+											<a href="<?= $menu->url; ?>" target="<?= $menu->target; ?>"><?= $menu->title; ?></a>
+										</li>
+										<?php
+									}
+								?>
+								<li class="sm-link-wrap">
 									<?php
-										$menu_options = wp_get_nav_menu_items('landing navigation footer');
+										$menu_options = wp_get_nav_menu_items('landing social navigation');
 										foreach ((array) $menu_options as $menu) {
 											?>
-											<li>
-												<a href="<?= $menu->url; ?>" target="<?= $menu->target; ?>"><?= $menu->title; ?></a>
-											</li>
+											<a class="sm-link" href="<?= $menu->url; ?>" target="<?= $menu->target; ?>"><i class="fab fa-<?= $menu->title; ?>"></i></a>
 											<?php
 										}
 									?>
-									<li class="sm-link-wrap">
-										<?php
-											$menu_options = wp_get_nav_menu_items('landing social navigation');
-											foreach ((array) $menu_options as $menu) {
-												?>
-												<a class="sm-link" href="<?= $menu->url; ?>" target="<?= $menu->target; ?>"><i class="fab fa-<?= $menu->title; ?>"></i></a>
-												<?php
-											}
-										?>
-									</li>
+								</li>
 
-								</ul>
+							</ul>
 
-							</div>
 						</div>
 					</div>
 				</div>
-			</footer>
-		<?php
-			}
-		?>
+			</div>
+		</footer>
 
 
 		<!-- <footer id="colophon" class="site-footer"> -->
